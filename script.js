d781efdca5e95a1a7d10aa7c1fb16c466213e9b9
Louis Minsky <github@louisminsky.com>
2021-12-24 10:56:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("hello!");
+console.log(getData());
+
+function getData()
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "data", false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}

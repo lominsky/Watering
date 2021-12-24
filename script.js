c0356764f447781bc979261d5931cda4d87b0fa2
Louis Minsky <github@louisminsky.com>
2021-12-24 11:25:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("hello!");
-document.body.innerHTML = "<h1 id='name'></h1><p id='temp'></p><p id='cap'></p>";
-var intervalID = setInterval(updateData(), 30000);
+document.body.innerHTML = "<h1 id='name'></h1><h6 id='time' style='color:grey'></h6><p id='temp'></p><p id='cap'></p>";
+var intervalID = setInterval(updateData(), 10000);
           
 
 function getData()
@@ -13,7 +13,9 @@ function getData()
 
 function updateData() {
   let d = getData();  
+  let now = new Date();
   document.getElementById("name").innerText = d.name;
+  document.getElementById("time").innerText = "Updated " + now.toLocaleString();
   document.getElementById("temp").innerText = "Temperature: " + d.temp + "º C";
   document.getElementById("cap").innerText = "Capacitance (~100-2000): " + d.cap;
 }

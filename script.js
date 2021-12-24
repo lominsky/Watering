f8e8bf2ab4987b04770b5ced31153b58c6e16cba
Louis Minsky <github@louisminsky.com>
2021-12-24 11:08:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log("hello!");
-console.log(getData());
+document.body.append("<h1 id='name'></h1><p id='temp'></p><p id='cap'></p>");
+var intervalID = setInterval(updateData(), 30000);
+          
 
 function getData()
 {
@@ -7,4 +9,11 @@ function getData()
     xmlHttp.open( "GET", "data", false ); // false for synchronous request
     xmlHttp.send( null );
     return JSON.parse(xmlHttp.responseText);
+}
+
+function updateData() {
+  let d = getData();  
+  document.getElementById("name") = d.name;
+  document.getElementById("temp") = "Temperature: " + d.temp + "º C";
+  document.getElementById("temp") = "Capacitance (~100-2000): " + d.cap;
 }

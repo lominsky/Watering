eeaca9e71fe958cfe43afd1aa421c0bdeb5e9721
Louis Minsky <github@louisminsky.com>
2021-12-24 10:59:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ function getData()
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", "data", false ); // false for synchronous request
     xmlHttp.send( null );
-    return xmlHttp.responseText;
+    return JSON.parse(xmlHttp.responseText);
 }
